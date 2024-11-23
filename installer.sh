f0ed4dd19055e7faa1ca0a4542b9579453a698f1
Ibukun Soetan <160017075+h3llo-world-33@users.noreply.github.com>
2024-11-23 08:51:07 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,7 +2,7 @@
 # Java installation for jenkins
 
 sudo apt update
-sudo apt install openjdk-17-jre-headless 
+sudo apt install openjdk-17-jre-headless -y
 
 # Jenkins installation 
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
