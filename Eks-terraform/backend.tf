d105a40d26ba9410bb07d1c82c6b050831dcc62a
Ibukun Soetan <160017075+h3llo-world-33@users.noreply.github.com>
2024-12-05 20:17:53 +0100

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hello-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "cka-handson" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-2"
   }
